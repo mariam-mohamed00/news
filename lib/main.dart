06f6a8373d68f573a8e6772a7e9075b927833f28
mariam <mariemmohamed2030@gmail.com>
2024-12-26 21:16:13 +0200

    initial app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/my_theme.dart';
+import 'package:news_app/routing/app_router.dart';
+import 'package:news_app/routing/routes.dart';
+
+void main()  {
+
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.home,
+      onGenerateRoute: AppRouter.generateRoute,
+      theme: MyTheme.lightMode,
+      darkTheme: MyTheme.darkMode,
+      themeMode: ThemeMode.system,
+    );
+  }
+
+
+}
---
+++ lib/routing/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/routing/routes.dart';
+
+import '../screens/home_screen.dart';
+
+class AppRouter {
+  static Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.home:
+        return MaterialPageRoute(
+            builder: (context) => const HomeScreen(), settings: settings);
+      
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const Scaffold(),
+        );
+    }
+  }
+}
